fa9aed46584406c53f542621551efae02182415b
alex (is a badger, an IRL badger) <73854333+alexisabadger@users.noreply.github.com>
2021-06-15 19:14:23 -0400

    Add files via upload

---
+++ hex.js
@@ -0,0 +1,100 @@
+// array to hold hexadecimal digits
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+// declare const(s) to access objects on the DOM
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+const spanColor = document.getElementById("bg-color");
+
+// testing onLoad event
+window.addEventListener('load', (event) => {
+    console.log('The page has fully loaded');
+
+    // call createHex to generate a hex color
+    let currentHexColor = createHexColor();
+
+    // set the textContent of the
+    spanColor.textContent = currentHexColor;
+
+    document.body.style.backgroundColor = currentHexColor;
+
+    // pass color HEX to RGB
+    let passHexColor = document.getElementById("bg-color").textContent;
+    sessionStorage.setItem("passHexColor", passHexColor);
+
+    // receiving color (from RGB)
+    var passRGBColor = sessionStorage.getItem("passRGBColor");
+    // if received value is null, alert user
+    if (passRGBColor == null) {
+        console.log("passRGBColor is currently null...");
+    } 
+    // if there's a received value, convert and apply to background
+    else {
+        console.log(passRGBColor);
+        // console.log("The hex value is: " + rgbToHex(passRGBColor));
+    }
+});
+
+// take the received RGB color string, break into R, G, B values
+function breakRGB(rgbString) {
+    
+}
+
+// RGB to hex conversion functions
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+  
+function rgbToHex(r, g, b) {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+
+
+
+// event listener for id="btn" (text: CLICKME)
+// --> every 'click' event calls this anonymous function:
+btn.addEventListener('click', function() {
+
+    // call createHex to generate a Hex color value
+    let currentHexColor = createHexColor();
+
+    // set the textContent of the 
+    color.textContent = currentHexColor;
+
+    // 
+    document.body.style.backgroundColor = currentHexColor;
+
+    // update session variable
+    passHexColor = currentHexColor;
+    sessionStorage.setItem("passHexColor", passHexColor);
+});
+
+// this function generates a random hex color value
+function createHexColor() {
+    // declare hexColor, set it equal to '#' at start (all hex colors need this)
+    let hexColor = '#';
+
+    // loop to add hex digits to hexColor
+    for (let i = 0; i < 6; i++) {
+        hexColor += hex[getRandomNumber()];
+    }
+
+    return hexColor;
+}
+
+// function to get a random number from 0 to F (hex[0] to hex[15])
+function getRandomNumber() {
+    return Math.floor(Math.random() * hex.length);
+}
+
+// TEST - passing variables between pages (RECEIVING: rgb to hex)
+// var favoriteMovie = sessionStorage.getItem("favoriteMovie");
+// if (favoriteMovie != null){
+//     console.log(favoriteMovie);
+// }
+
+// TEST - passing color between pages (PASSING: hex to rgb)
+// var secondFavoriteMovie = "Finding Nemo";
+// sessionStorage.setItem("secondFavoriteMovie", secondFavoriteMovie);
---
+++ rgb.js
@@ -0,0 +1,140 @@
+// array to hold hexadecimal digits
+const hex = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
+
+// declare const(s) to access objects on the DOM
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+const spanColor = document.getElementById("bg-color");
+
+// testing onLoad event
+window.addEventListener('load', (event) => {
+    // console.log('The page has fully loaded');
+    console.log("page load event listener is firing");
+    // receiving color (from HEX page)
+    var passHexColor = sessionStorage.getItem("passHexColor");
+    var currentRGBColor = '';
+
+    // if/else to see if there's a stored value coming in
+    // if received value is null, alert user AND generate a new color
+    if (passHexColor == null) {
+        console.log("pageLoad -> if statement is firing");
+        console.log("(passHexColor is currently null...)");
+
+        // call createRGB to generate an RGB color
+        currentRGBColor = createRGB();
+
+        spanColor.textContent = currentRGBColor;
+
+        document.body.style.backgroundColor = currentRGBColor;
+    } 
+    // if there's a received value, convert and apply to background
+    else {
+        console.log("pageLoad - > else statement is firing");
+        console.log(passHexColor);
+        // console.log(hexToRGB(passHexColor));
+
+        currentRGBColor = hexToRGB(passHexColor);
+
+        spanColor.textContent = currentRGBColor;
+
+        document.body.style.backgroundColor = currentRGBColor;
+    }
+
+    // pass color RGB to HEX
+    let passRGBColor = document.getElementById("bg-color").textContent;
+    sessionStorage.setItem("passRGBColor", passRGBColor);
+
+});
+
+// this function converts a 6-digit hexadecimal color to its equivilant RGB value
+function hexToRGB(hexInc) {
+    console.log("hexToRGB is firing");
+    // get individual red, green, and blue values
+    let hexR = hexInc.slice(1, 3);
+    let hexG = hexInc.slice(3, 5);
+    let hexB = hexInc.slice(5, 7);
+
+    // convert to RGB equivilants
+    let rgbR = hexConvert(hexR);
+    let rgbG = hexConvert(hexG);
+    let rgbB = hexConvert(hexB);
+
+    // construct RGB value as string
+    let newRGBColor = "rgb (" + rgbR + ", " + rgbG +
+                      ", " + rgbB + ")";
+
+    // return to caller
+    return newRGBColor;
+}
+
+// to convert a two-digit hex to decimal
+function hexConvert(incomingHexColor) {
+    console.log("hexConvert is firing");
+    // turn two-digit hexademical number into a string of single hex digits
+    let hexSingles = Array.from(incomingHexColor);     // let or var?
+
+    // convert each to binary, add to get RGB value
+    let newRGB = ((hex.indexOf(hexSingles[0]) * 16**1) + (hex.indexOf(hexSingles[1]) * 16**0));
+    console.log(newRGB);
+
+    return newRGB;
+}
+
+
+
+// event listener for id="btn" (text: CLICKME)
+// --> every 'click' event calls this anonymous function:
+btn.addEventListener('click', function() {
+    console.log("anonymous button event listener is firing");
+    
+    // call createRGB to generate an RGB color
+    let currentRGBColor = createRGB();
+
+    // set the textContent of the 
+    color.textContent = currentRGBColor;
+
+    // 
+    document.body.style.backgroundColor = currentRGBColor;
+
+    // update session variable
+    passRGBColor = currentRGBColor;
+    sessionStorage.setItem("passRGBColor", passRGBColor);
+
+});
+
+// this function generates a random RGB color value
+function createRGB() {
+    console.log("createRGB is firing");
+    // declare hexColor, set it equal to '#' at start (all hex colors need this)
+    let rColor = '';
+    let gColor = '';
+    let bColor = '';
+
+    // get random numbers between 0 and 255
+    rColor += getRandomNumber();
+    gColor += getRandomNumber();
+    bColor += getRandomNumber();
+
+    // combine RGB values
+    let myRGBColor = 'rgb(' + rColor + ', ' + gColor + ', ' +
+                    bColor + ')';
+
+    return myRGBColor;
+}
+
+// function to get a random number from 0 to F (hex[0] to hex[15])
+function getRandomNumber() {
+    console.log("getRandomNumber is firing");
+    return Math.floor(Math.random() * 256);
+}
+
+
+// TEST - passing variable between pages (PASSING: RGB to Hex)
+// var favoriteMovie = "Shrek";
+// sessionStorage.setItem("favoriteMovie", favoriteMovie);
+
+// TEST - passing variable between pages (RECEIVING: hex to RGB)
+// var secondFavoriteMovie = sessionStorage.getItem("secondFavoriteMovie");
+// if (secondFavoriteMovie != null){
+//     console.log(secondFavoriteMovie);
+// }
